babddcd6953d6a146910383226dc565275be45f9
Peter Cooper <git@peterc.org>
2014-08-02 18:21:04 +0100

    hooked up websocket stuff

--- hello.js
+++ hello.js
@@ -3,17 +3,45 @@ var logfmt = require('logfmt');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
+// Express middleware
+app.use(logfmt.requestLogger());
+app.use(express.static('public'));
+
+// Stores messages waiting to go out over WebSocket
 var messageQueue = [];
 
-app.use(logfmt.requestLogger());
 
+
+// Process an SMS coming in
 app.get('/sms', function(req, res) {
+  var message = {};
+  message.from = req.param('From');
+  message.body = req.param('Body');
+  messageQueue.push(message);
+  eventEmitter.emit('message', message);
+
   res.set('Content-Type', 'text/xml');
-  res.send('<?xml version="1.0" encoding="UTF-8"?>\
-<Response><Message>Received ' + req.param('From') + ' ' + req.param('Body') + '</Message></Response>');
+  res.send('<?xml version="1.0" encoding="UTF-8"?><Response></Response>');
 });
 
+
+// WebSocket stuff
+io.on('connection', function(socket) {
+
+  eventEmitter.on('message', function(message) {
+    socket.emit('message', message );
+  });
+
+  console.log('connection!');
+  socket.on('event', function(data) {});
+  socket.on('disconnect', function() {});
+});
+
+
+// Start server
 var port = Number(process.env.PORT || 5000);
 server.listen(port, function() {
   console.log("Listening on " + port);
