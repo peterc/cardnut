5d83feb1c6618cb0bc9816f3dd339163ec3e6907
Peter Cooper <git@peterc.org>
2014-08-02 17:12:04 +0100

    boing

--- hello.js
+++ hello.js
@@ -9,8 +9,8 @@ var messageQueue = [];
 app.use(logfmt.requestLogger());
 
 app.get('/sms', function(req, res) {
-  console.log(req.query.From);
-  console.log(req.params.Body);
+  console.log(req.param('From'));
+  console.log(req.param('Body'));
   res.set('Content-Type', 'text/xml');
   res.send('<?xml version="1.0" encoding="UTF-8"?><Response></Response>');
 });
