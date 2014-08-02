0144df54067eaa03d1d9eddda8250046ed2b4b98
Peter Cooper <git@peterc.org>
2014-08-02 15:20:57 +0100

    Such magic

--- hello.js
+++ hello.js
@@ -1,15 +1,21 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
+var logfmt = require('logfmt');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var messageQueue = [];
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get('/sms', function(req, res) {
+  console.log(req.params['From']);
+  console.log(req.params['Body']);
+  res.set('Content-Type', 'text/xml');
+  res.send('<?xml version="1.0" encoding="UTF-8"?><Response></Response>');
 });
 
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
+server.listen(port, function() {
   console.log("Listening on " + port);
 });
